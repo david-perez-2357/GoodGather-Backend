cd0f3296f14e4479f169641ce4e42b7496d37edb
david <dperezfernandez@safareyes.es>
2024-11-07 10:12:32 +0100

    🎉 Initialize project

---
+++ src/main/java/main/goodgatherbackend/GoodGatherBackendApplication.java
@@ -0,0 +1,13 @@
+package main.goodgatherbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodGatherBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoodGatherBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/main/goodgatherbackend/GoodGatherBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package main.goodgatherbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodGatherBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
