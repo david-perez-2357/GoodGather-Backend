e3f0ecc1dc4a4297840001b4ec5a9372b0772009
pablorodbriguezbravo <prodriguezbravo@safareyes.es>
2024-11-08 17:07:15 +0100

    ✨ Repositories created

---
+++ src/main/java/repositories/EventRepository.java
@@ -0,0 +1,7 @@
+package repositories;
+
+import models.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EventRepository extends JpaRepository<Event, Integer> {
+}
