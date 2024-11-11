c9aca6130c2300cd6e1e7d18b1434e83cb8aa247
david <dperezfernandez@safareyes.es>
2024-11-11 15:05:17 +0100

    🩹 Add repository annotation

--- src/main/java/main/goodgatherbackend/repositories/EventRepository.java
+++ src/main/java/main/goodgatherbackend/repositories/EventRepository.java
@@ -2,6 +2,8 @@ package main.goodgatherbackend.repositories;
 
 import main.goodgatherbackend.models.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EventRepository extends JpaRepository<Event, Integer> {
 }
--- src/main/java/main/goodgatherbackend/repositories/TicketRepository.java
+++ src/main/java/main/goodgatherbackend/repositories/TicketRepository.java
@@ -2,6 +2,8 @@ package main.goodgatherbackend.repositories;
 
 import main.goodgatherbackend.models.Ticket;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TicketRepository extends JpaRepository<Ticket, Integer> {
 }
