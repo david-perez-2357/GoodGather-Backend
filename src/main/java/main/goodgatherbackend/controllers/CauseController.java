f0f6a6cee77311692b58c4aff9b615cd699d310a
Jorge Arias <jariasmartin@safareyes.es>
2024-11-12 16:43:08 +0100

    ✨ Create all controllers with decorators

---
+++ src/main/java/main/goodgatherbackend/controllers/CauseController.java
@@ -0,0 +1,11 @@
+package main.goodgatherbackend.controllers;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/cause")
+@AllArgsConstructor
+public class CauseController {
+}
---
+++ src/main/java/main/goodgatherbackend/controllers/TicketController.java
@@ -0,0 +1,11 @@
+package main.goodgatherbackend.controllers;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ticket")
+@AllArgsConstructor
+public class TicketController {
+}
