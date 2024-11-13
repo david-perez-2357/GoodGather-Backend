aaa1d9d25d15790957dedd84db6cca5d3554408d
david <dperezfernandez@safareyes.es>
2024-11-13 11:07:03 +0100

    ✨ Created service get methods

--- src/main/java/main/goodgatherbackend/services/CauseService.java
+++ src/main/java/main/goodgatherbackend/services/CauseService.java
@@ -1,9 +1,22 @@
 package main.goodgatherbackend.services;
 
 import lombok.AllArgsConstructor;
+import main.goodgatherbackend.dtos.CauseDTO;
+import main.goodgatherbackend.mappers.CauseMapper;
+import main.goodgatherbackend.models.Cause;
+import main.goodgatherbackend.repositories.CauseRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @AllArgsConstructor
 public class CauseService {
+    private CauseRepository causeRepository;
+    private CauseMapper causeMapper;
+
+    public List<CauseDTO> getAll() {
+        List<Cause> causes = causeRepository.findAll();
+        return causeMapper.toDTOList(causes);
+    }
 }
