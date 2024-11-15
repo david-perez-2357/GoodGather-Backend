86703c6df71b55fd7c63fcb4b73b9d77aae17476
DavidClase <dperezfernandez@safareyes.es>
2024-11-15 20:26:49 +0100

    ✨ New service and controller to get 24h tickets

--- src/main/java/main/goodgatherbackend/services/TicketService.java
+++ src/main/java/main/goodgatherbackend/services/TicketService.java
@@ -3,20 +3,31 @@ package main.goodgatherbackend.services;
 import lombok.AllArgsConstructor;
 import main.goodgatherbackend.dtos.TicketDTO;
 import main.goodgatherbackend.mappers.TicketMapper;
+import main.goodgatherbackend.models.Event;
 import main.goodgatherbackend.models.Ticket;
+import main.goodgatherbackend.repositories.EventRepository;
 import main.goodgatherbackend.repositories.TicketRepository;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
 @AllArgsConstructor
 public class TicketService {
     private TicketRepository ticketRepository;
+    private EventRepository eventRepository;
     private TicketMapper ticketMapper;
 
     public List<TicketDTO> getAll() {
         List<Ticket> tickets = ticketRepository.findAll();
+        return ticketMapper.toDTOList(tickets);
+    }
+
+    public List<TicketDTO> get24hBoughtTickets(Integer eventId) {
+        Event event = eventRepository.findById(eventId).orElseThrow();
+        LocalDateTime yesterday = LocalDateTime.now().minusDays(1);
+        List<Ticket> tickets = ticketRepository.findAllByEventAndPurchaseDateIsAfter(event, yesterday);
         return ticketMapper.toDTOList(tickets);
     }
 }
