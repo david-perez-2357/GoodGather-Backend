bbb4d6b594b69c9b7e13d39c20ea400f2b756cc8
Jorge Arias <jariasmartin@safareyes.es>
2024-11-19 19:17:56 +0100

    ✨ Create getById function

--- src/main/java/main/goodgatherbackend/controllers/CauseController.java
+++ src/main/java/main/goodgatherbackend/controllers/CauseController.java
@@ -2,8 +2,11 @@ package main.goodgatherbackend.controllers;
 
 import lombok.AllArgsConstructor;
 import main.goodgatherbackend.dtos.CauseDTO;
+import main.goodgatherbackend.dtos.EventDTO;
 import main.goodgatherbackend.services.CauseService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,5 +21,14 @@ public class CauseController {
     @GetMapping
     public List<CauseDTO> getAll() {
         return causeService.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CauseDTO> getById(@PathVariable Integer id) {
+        try {
+            return ResponseEntity.ok(causeService.getById(id));
+        } catch (Exception e) {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
