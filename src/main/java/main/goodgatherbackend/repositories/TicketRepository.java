60f0c51d1b91f7a3cc7e444904efc396ccafd889
david <dperezfernandez@safareyes.es>
2024-11-24 16:04:32 +0100

    ✨ Create new method in ticket repository

--- src/main/java/main/goodgatherbackend/repositories/TicketRepository.java
+++ src/main/java/main/goodgatherbackend/repositories/TicketRepository.java
@@ -17,4 +17,6 @@ public interface TicketRepository extends JpaRepository<Ticket, Integer> {
 
     @Query("SELECT SUM(t.amount) FROM Ticket t WHERE t.event.id = :eventId")
     Integer sumAmountByEventId(Integer eventId);
+
+    List<Ticket> findByEventIdAndUserId(Integer eventId, Integer userId);
 }
