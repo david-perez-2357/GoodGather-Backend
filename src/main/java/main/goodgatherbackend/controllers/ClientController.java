79b713a1cbf1b3e0dc13ff665c8f9b4b90e90cbd
pablorodbriguezbravo <prodriguezbravo@safareyes.es>
2024-11-28 17:24:20 +0100

    ⚡️ Add log in, log out, register functionality

--- src/main/java/main/goodgatherbackend/controllers/ClientController.java
+++ src/main/java/main/goodgatherbackend/controllers/ClientController.java
@@ -1,13 +1,21 @@
 package main.goodgatherbackend.controllers;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import main.goodgatherbackend.dtos.ClientDTO;
 import main.goodgatherbackend.dtos.UserClientDTO;
+import main.goodgatherbackend.models.User;
+import main.goodgatherbackend.security.auth.AuthenticationRequest;
+import main.goodgatherbackend.security.auth.AuthenticationResponse;
+import main.goodgatherbackend.security.auth.AuthenticationService;
+import main.goodgatherbackend.security.auth.RegisterRequest;
 import main.goodgatherbackend.services.ClientService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import main.goodgatherbackend.services.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -15,6 +23,8 @@ import java.util.List;
 @RequestMapping("/client")
 @AllArgsConstructor
 public class ClientController {
+    private final AuthenticationService authenticationService;
+    private final UserService userService;
     private ClientService clientService;
 
     @GetMapping()
@@ -26,4 +36,40 @@ public class ClientController {
     public UserClientDTO getById(@PathVariable Integer id) {
         return clientService.findById(id);
     }
+
+    @PostMapping()
+    public ResponseEntity<String> createUser(@RequestBody UserClientDTO userClientDTO) {
+
+        try {
+            RegisterRequest registerRequest = new RegisterRequest();
+            registerRequest.setUsername(userClientDTO.getUsername());
+            registerRequest.setPassword(userClientDTO.getPassword());
+            authenticationService.register(registerRequest);
+
+                    clientService.saveClient(userClientDTO);
+            return ResponseEntity.status(HttpStatus.CREATED).body("User and client created successfully!");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error creating user or client: " + e.getMessage());
+
+        }
+
+
+    }
+
+//    @PostMapping()
+//    public ResponseEntity<AuthenticationResponse> doLogin(@RequestBody AuthenticationRequest authenticationRequest){
+//        try {
+//            AuthenticationResponse authenticationResponse = authenticationService.authenticate(authenticationRequest);
+//            return ResponseEntity.ok(authenticationResponse);
+//        } catch (Exception e) {
+//            return ResponseEntity.status(401).body(null);
+//        }
+//    }
+
+
+
+
+
+
 }
--- src/main/java/main/goodgatherbackend/services/ClientService.java
+++ src/main/java/main/goodgatherbackend/services/ClientService.java
@@ -6,8 +6,11 @@ import main.goodgatherbackend.dtos.UserClientDTO;
 import main.goodgatherbackend.mappers.ClientMapper;
 import main.goodgatherbackend.mappers.UserClientMapper;
 import main.goodgatherbackend.models.Client;
+import main.goodgatherbackend.models.User;
 import main.goodgatherbackend.repositories.ClientRepository;
+import main.goodgatherbackend.repositories.UserRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -17,6 +20,8 @@ public class ClientService {
     private ClientRepository clientRepository;
     private ClientMapper clientMapper;
     private UserClientMapper userClientMapper;
+    private UserRepository userRepository;
+
 
     public List<ClientDTO> getAll() {
         List<Client> clients = clientRepository.findAll();
@@ -26,5 +31,15 @@ public class ClientService {
     public UserClientDTO findById(Integer idClient) {
         Client client = clientRepository.findById(idClient).orElse(null);
         return userClientMapper.toDTO(client);
+    }
+
+
+    public void saveClient(UserClientDTO userClientDTO) {
+        User user = userRepository.findByUsername(userClientDTO.getUsername())
+                .orElseThrow(() -> new RuntimeException("User not found"));
+        Client client = userClientMapper.toClient(userClientDTO);
+        client.setUser(user);
+
+        clientRepository.save(client);
     }
 }
