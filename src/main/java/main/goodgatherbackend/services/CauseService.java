2bc6b55c8c3b032d02977541ed02de43ca8f5e7d
Jorge Arias <jariasmartin@safareyes.es>
2024-12-03 19:13:07 +0100

    ✨ Get All Events from a cause.

--- src/main/java/main/goodgatherbackend/services/CauseService.java
+++ src/main/java/main/goodgatherbackend/services/CauseService.java
@@ -2,11 +2,14 @@ package main.goodgatherbackend.services;
 
 import lombok.AllArgsConstructor;
 import main.goodgatherbackend.dtos.CauseDTO;
+import main.goodgatherbackend.dtos.EventDTO;
 import main.goodgatherbackend.mappers.CauseMapper;
+import main.goodgatherbackend.mappers.EventMapper;
 import main.goodgatherbackend.models.Cause;
 import main.goodgatherbackend.models.Event;
 import main.goodgatherbackend.models.Ticket;
 import main.goodgatherbackend.repositories.CauseRepository;
+import main.goodgatherbackend.repositories.EventRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,8 +17,10 @@ import java.util.List;
 @Service
 @AllArgsConstructor
 public class CauseService {
+    private final EventRepository eventRepository;
     private CauseRepository causeRepository;
     private CauseMapper causeMapper;
+    private EventMapper eventMapper;
 
     public List<CauseDTO> getAll() {
         List<Cause> causes = causeRepository.findAll();
@@ -34,4 +39,12 @@ public class CauseService {
         System.out.println("Tickets: " + tickets.size());
         return tickets.stream().mapToDouble(Ticket::getPrice).sum();
     }
+
+    // Quiero un metodo que coja todos los eventos de una causa utilizando el metodo del causeRepository
+    public List<EventDTO> getEventsFromCause(Integer id) {
+        Cause cause = causeRepository.findById(id).orElseThrow();
+        List<Event> events = eventRepository.findAllByCause(cause);
+        return eventMapper.toDTOList(events);
+    }
+
 }
