1115d0b277760bb7173343d03bea78a871cfc68b
david-perez-2357 <149081133+david-perez-2357@users.noreply.github.com>
2024-12-09 14:37:03 +0100

    ♻️ Change route of user controller

--- src/main/java/main/goodgatherbackend/controllers/UserController.java
+++ src/main/java/main/goodgatherbackend/controllers/UserController.java
@@ -5,10 +5,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/user")
 @AllArgsConstructor
 @Validated
 public class UserController {
-
-
 }
